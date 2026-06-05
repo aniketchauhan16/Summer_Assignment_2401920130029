b328ba9b21c999f2954f06f08d35dd5aa2421361
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-06-06 00:21:16 +0530

    Commited Diagonal Sum Solution

---
+++ Week_1/Day_4/DiagonalSum.java
@@ -0,0 +1,15 @@
+package Week_1.Day_4;
+
+public class DiagonalSum {
+       public int diagonalSum(int[][] mat) {
+        int sum =0;
+        for(int i=0;i<mat.length;i++){
+            sum += mat[i][i];
+            if(i != mat.length -1 -i){ 
+                sum += mat[i][mat.length -1 -i];
+            }
+        }
+        return sum;
+    }
+    
+}
