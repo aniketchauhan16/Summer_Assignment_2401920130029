c324a185664c3a457ef115c0cd0fb7ea48b62a9a
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-06-09 23:28:19 +0530

    Week 2 Day 2 Work commited

---
+++ Week_2/Day_2/AllAnagrams.java
@@ -0,0 +1,56 @@
+package Week_2.Day_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllAnagrams {
+      public List<Integer> findAnagrams(String s, String p) {
+        ArrayList<Integer> l=new ArrayList<>();
+        if(s.length()<p.length()){
+            return l;
+        }
+        int[] a=new int[26];
+        for(int i=0;i<p.length();i++){
+            a[p.charAt(i)-'a']++;
+        }
+        int q=0;
+        for(int i=0;i<26;i++){
+            if(a[i]!=0){
+                q++;
+            }
+        }
+        int i=0;
+        int j=p.length()-1;
+        int[] b=new int[26];
+        for(int k=i;k<=j;k++){
+            b[s.charAt(k)-'a']++;
+        }
+        int x=0;
+        while(j<s.length()-1){
+            x=0;
+            for(int k=0;k<26;k++){
+                if(a[k]!=0&&a[k]==b[k]){
+                    x++;
+                }
+            }
+            if(q==x){
+                l.add(i);
+            }
+            i++;
+            j++;
+            b[s.charAt(j)-'a']++;
+            b[s.charAt(i-1)-'a']--;
+        }
+        x=0;
+        for(int k=0;k<26;k++){
+            if(a[k]!=0&&a[k]==b[k]){
+                x++;
+            }
+        }
+        if(q==x){
+            l.add(i);
+        }
+        return l;
+    }
+    
+}
