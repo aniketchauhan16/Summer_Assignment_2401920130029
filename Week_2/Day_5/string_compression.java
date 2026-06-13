6590600a4a9ce40df5acdcdcb43c0ddf8212a483
Aniket Chauhan <aniketchauhan4163@gmail.com>
2026-06-13 23:33:45 +0530

    Week 2 day 5 work commited

---
+++ Week_2/Day_5/string_compression.java
@@ -0,0 +1,28 @@
+package Week_2.Day_5;
+
+public class string_compression {
+      public int compress(char[] chars) {
+        int n = chars.length;
+        int index = 0;
+        for (int i = 0; i < n;) {
+            char current = chars[i];
+            int count = 0;
+
+            while (i < n && chars[i] == current) {
+                i++;
+                count++;
+            }
+
+            chars[index++] = current;
+
+            // if count > 1, write the digits
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[index++] = c;
+                }
+            }
+        }
+
+        return index; 
+    }
+}
